4f276398bda319d057747c7da36f819feef1f9d8
Lee Worden <worden.lee@gmail.com>
2016-01-25 11:49:26 -0800

    add .step rules to site dir

---
+++ sage.mk
@@ -0,0 +1,33 @@
+ifeq ($(HOME),)
+export HOME = /usr/local/workingwiki/working-directories
+export DOT_SAGE = $(HOME)/.sage
+export SAGE_ROOT = /usr/share/sage
+export LD_LIBRARY_PATH += :/usr/share/sage/local/lib
+export SAGE_DOC = /usr/share/sage/devel/doc
+endif
+
+# how to run a sage script and capture its output
+# the odd tee command is possibly temporary - it puts the 
+# script's output into the .make.log (or console) as well as the output file
+%.sage.out : %.sage
+	$(RM) $*.sage.out $*.sage.status $(STEP_PRODUCTS)
+	(sage $< && touch $*.sage.status) | tee $*.sage.dmp
+	[ -e $*.sage.status ] && ($(RM) $*.sage.status && mv $*.sage.dmp $*.sage.out) || exit 1
+
+# .tried file gets updated if you just TRY to make the .out file
+# use this as a downstream prerequisite to avoid lots of hopeless retrying
+%.sage.tried : %.sage
+	touch $*.sage.tried
+	-$(MAKE) $*.sage.out
+
+.PRECIOUS: %.sage.tried
+
+# and a convenience rule to invoke sage interactively, but using all the
+# exports from the projects' makefiles
+sage :
+	sage
+
+sage-gdb :
+	sage -gdb
+
+.PHONY: sage sage-gdb
---
+++ step.pl
@@ -0,0 +1,21 @@
+#!/usr/local/bin/perl
+my($stem, $commentmark) = @ARGV;
+open STEP, "$stem.step" or die "opening $stem.step";
+open MK, ">$stem.mk" or die "opening $stem.mk";
+open SCRIPT, ">$stem" or die "opening $stem";
+print MK "# helper makefile automatically generated from $stem.step\n";
+my $products = '';
+while (<STEP>)
+{ if (/$commentmark\s*requires:\s*(\S.*)$/i) {
+    print MK "$stem $stem.out $stem.tried : $1\n";
+  }
+  elsif (/$commentmark\s*produces:\s*(\S.*)$/i) {
+    #print MK "$1 : $stem.tried ;\n";
+    print MK "$1 : $stem.out ;\n";
+    $products .= " $1";
+  }
+  print SCRIPT;
+}
+if ($products) {
+  print MK "$stem $stem.out $stem.tried : STEP_PRODUCTS=$products\n";
+}
